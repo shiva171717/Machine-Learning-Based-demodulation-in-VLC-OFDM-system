c1b94d3c35512882bc38f50c75c53878a71d9ab1
shiva171717 <115244540+shiva171717@users.noreply.github.com>
2024-11-11 15:23:08 +0530

    Update pnseq11.m

--- pnseq11.m
+++ pnseq11.m
@@ -1,15 +1,1 @@
-function out=pnseq11(N)
-m=11;  % No. of stages
-%length of the sequence
 
-
-
-% taps = [1 (((sign(randn(1,m-1)))+1)./2)];
-
-taps=[1 0 1 1 1 0 0 1 0 1 1];
-for J=1:N   
-    out(J)=taps(m);
-    temp2=xor(taps(11),taps(9));
-    temp=taps(1:length(taps)-1);
-    taps=[temp2 temp];
-end
